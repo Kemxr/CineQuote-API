40825d6a280e1c2fc8c77562a41f101fe185c540
vecval24 <valentine.vecchi@heig-vd.ch>
2025-12-01 12:07:15 +0100

    PageViews création et path router

--- frontend/cine-quote-frontend/router.js
+++ frontend/cine-quote-frontend/router.js
@@ -2,7 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LoginForm from './src/components/Auth/LoginForm.vue';
 import HomeView from './src/views/HomeView.vue';
 import RegisterForm from './src/components/Auth/RegisterForm.vue';
-
+import Explore from './src/views/Explore.vue';
+import Favorites from './src/views/Favorites.vue';
+import Profile from './src/views/Profile.vue';
+import Quiz from './src/views/Quiz.vue';
 
 const routes = [
   {
@@ -19,6 +22,26 @@ const routes = [
     path: '/register',
     name: 'RegisterForm',
     component: RegisterForm
+  },
+  {
+    path: '/explore',
+    name: 'Explore',
+    component: Explore
+  },
+  {
+    path: '/favorites',
+    name: 'Favorites',
+    component: Favorites
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/quiz',
+    name: 'Quiz',
+    component: Quiz
   }
 ];
 
